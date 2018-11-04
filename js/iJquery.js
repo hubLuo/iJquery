443976b614708374f186f4f8cfe0582b127ab094
hubLuo <hubluo@126.com>
2018-11-04 20:40:30 +0800

    4.基础架构：2.2.对象分类管理：扩展方式-浅拷贝

--- js/iJquery.js
+++ js/iJquery.js
@@ -24,7 +24,33 @@
     carrier.fn=carrier.prototype.init.prototype=carrier.prototype;//原型链共享
     //JQ挂载extend方法
     carrier.fn.extend=carrier.extend=function(){
-        console.log(this);
+        /*需求
+         * 1：参数1---扩展对象：实例扩展，实例原型扩展，本身扩展
+         * 2：参数2以上 任意对象扩展，扩展对象：arguments[0]
+         * 3：目标对象必须为引用类型，参数不能为空
+         * */
+        //需求3
+        var length=arguments.length,i= 1,target=arguments[0]||{},
+            option,//具体的src
+            name;//src对象名
+
+        if(typeof target!=="object"){
+            target={};
+        }
+        //需求1
+        if (length ==i){
+            target=this;//把this变为目标元素
+            i--;//把第一个元素变为src
+        }
+        //需求2
+        for(;i<length;i++){
+            if((option=arguments[i])!==null){
+                for(name in option ){
+                    target[name]=option[name];
+                }
+            };
+        }
+        return target;
     };
     root[name]=root.carrier=carrier;//别名
 },"$");
