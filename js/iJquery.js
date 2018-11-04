879b0b49cd57a828dfb4def070f795acdf783230
hubLuo <hubluo@126.com>
2018-11-05 00:42:52 +0800

    6.基础架构：2.4.对象分类管理：优化-extend做对象管理copy做深浅拷贝

--- js/iJquery.js
+++ js/iJquery.js
@@ -27,62 +27,77 @@
     };
     //carrier.prototype.init.prototype=carrier.prototype;//原型链共享//这样new init时，实例原型将指向carrier原型。
     carrier.fn=carrier.prototype.init.prototype=carrier.prototype;//原型链共享
-    //JQ挂载extend方法
+    //添加深浅拷贝静态方法
+    carrier.copy=function(deep,target,src){
+        var deep =deep||false,target=target,src=src,
+            toString=Object.prototype.toString,strArr="[object Array]",strObj="[object Object]";
+        if(typeof deep!=="boolean"){
+            if(target){
+                src=target,target=deep;
+            }else{
+                target=deep;
+            }
+            deep=false;
+        }
+        if(src==undefined){
+            src=target,target=undefined;
+        }
+        // 只拷贝对象
+        if(typeof src !=="object" ){
+            return false;
+        }
+        // 如果没有目标对象则 根据src的类型判断是新建一个数组还是对象
+        if(target==undefined){
+            target=src instanceof Array?[]:{};
+        }
+        for (var key in src) {
+            var isArray=false;
+            // 遍历src，并且判断是src的属性才拷贝
+            if (src.hasOwnProperty(key)) {
+                //deep为true且src属性值为对象和数组时进行递归，否则直接赋值。
+                if(deep&&(toString.call(src[key])==strObj||(isArray=toString.call(src[key])==strArr))){
+                    target[key]=isArray?(target[key] && toString.call(target)==strArr ?target[key]:[]):
+                        (target[key] && toString.call(target)==strObj ?target[key]:{});
+                    carrier.copy(deep,target[key],src[key]);
+                }else{
+                    target[key]=src[key];
+                }
+                //target[key] = typeof src[key] === 'object' ?carrier.deepCopy(src[key]) : src[key];
+            }
+        }
+        return target;
+    };
+    //JQ挂载extend方法：对象分类管理
     carrier.fn.extend=carrier.extend=function(){
         /*需求
-         * 1：参数1---扩展对象：实例扩展，实例原型扩展，本身扩展
-         * 2：参数2以上 任意对象扩展，扩展对象：arguments[0]
+         * 1：一个对象--扩展对象为this：实例扩展，实例原型扩展，本身扩展
+         * 2：二个对象及以上--扩展对象为任意无关对象：第一顺位对象为目标对象
          * 3：目标对象必须为引用类型，参数不能为空
          * */
-        //需求3
-        var length=arguments.length,i= 1,target=arguments[0]||{},
-            option,//具体的src
-            name;//src对象名
-        //深拷贝
-        var deep=false,//判断拷贝类型
-            copy,//记录option[name]，判断类型；
-            src,//记录target[name],判断类型；
-            copyIsArray,//记录当copy为数组时；
-            clone;//目标值的备份；
+        var length=arguments.length,i= 1,target=arguments[0]||{};
+        //深浅拷贝
+        var deep=false;
 
         if(typeof target=="boolean"){
             deep=target;
             target=arguments[1];
             i=2;
         }
-
-        if(typeof target!=="object"){
+        /*需求3: object情况：低版本IE的function，Object对象，数组，null
+                 function情况：较高版本浏览器都可以*/
+        if(typeof target!=="object" && typeof target !=="function"){
             target={};
         }
 
-        //需求1
+        //需求1和2
         if (length ==i){
             target=this;//把this变为目标元素
             i--;//把第一个元素变为src
         }
-        //需求2
+        //扩展：对象属性拷贝
         for(;i<length;i++){
-            if((option=arguments[i])!==null){
-                for(name in option ){
-                    src=target[name];
-                    copy= option[name];
-                    if(deep && (carrier.isPlainObject(copy) || (copyIsArray=carrier.isArray(copy)))){
-                        if(copyIsArray){
-                            //true被拷贝的值为数组
-                            copyIsArray=false;
-                            clone = src && carrier.isArray(src) ? src : [];
-                        }else{
-                            //被拷贝的值为对象
-                            clone = src && carrier.isPlainObject(src) ? src : {};
-                        }
-                        //深拷贝
-                        target[name] = carrier.extend( deep, clone, copy  );
-                    }else if(copy !==undefined){
-                        //浅拷贝
-                        //target[name]=option[name];
-                        target[name]=copy;
-                    }
-                }
+            if((arguments[i])!==null){
+                carrier.copy(deep,target,arguments[i]);
             };
         };
         return target;
