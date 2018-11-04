72515b1fcf3ef4cbab4e64aba9b031b3bee462e0
hubLuo <hubluo@126.com>
2018-11-04 20:27:39 +0800

    1.基础架构：1.2.共享原型链：init-构造函数：封装实例化操作（语法糖封装）$();

--- js/iJquery.js
+++ js/iJquery.js
@@ -8,6 +8,18 @@
     factory(root,name);
 }(this,function(root,name){
     var carrier=function(){ //封装构造函数
+        return new carrier.prototype.init();//创建carrier实例
     };
+    carrier.prototype={//carrier原型
+        constructor:carrier,
+            init:function(){
+            this[0]="LQ";
+            this[1]="is good";
+            return this;
+        },
+        css:function(){console.log("修改样式")},
+        pushStack:function(){console.log("constructor--",this.constructor());}
+    };
+    carrier.prototype.init.prototype=carrier.prototype;//原型链共享//这样new init时，实例原型将指向carrier原型。
     root[name]=root.carrier=carrier;//别名
 },"$");
